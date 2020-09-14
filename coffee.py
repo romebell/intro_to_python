31f3efb91f2c412b7d16e4e584d1a3a661e9ede2
Rome Bell <romebell5@gmail.com>
2020-09-14 14:09:55 -0700

    [coffee] Add CoffeeCup class and methods

---
+++ coffee.py
@@ -0,0 +1,16 @@
+class CoffeeCup():
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.amount = 0
+
+    def fill(self):
+        self.amount = self.capacity
+
+    def empty(self):
+        self.amount = 0
+    
+    def drink(self, amount):
+        self.amount -= amount
+        if (self.amount == 0):
+            self.amount = 0
+            print("Coffee cup is now empty")
